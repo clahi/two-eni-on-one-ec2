ca59e1f32cb23f76ad62f7d7181c398ee34344c3
Abdala <clahimoha1000@gmail.com>
2024-05-28 11:32:27 +0300

    attached the first network interface

--- main.tf
+++ main.tf
@@ -54,3 +54,72 @@ resource "aws_route_table_association" "route-table-assoc" {
   route_table_id = aws_route_table.public-route-table.id
 }
 
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = file("${path.module}/my-key.pub")
+}
+
+resource "aws_security_group" "network-security-group" {
+  name        = "allow_ssh"
+  description = "Allow ssh inbound traffic"
+  vpc_id      = aws_vpc.demo-vpc.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "nsg-inbound-ssh"
+  }
+}
+
+resource "aws_network_interface" "primary-network-interface" {
+  subnet_id       = aws_subnet.public-subnet.id
+  private_ips     = ["10.0.1.100"]
+  security_groups = [aws_security_group.network-security-group.id]
+
+  tags = {
+    Name = "primary-network-interface"
+  }
+}
+
+data "aws_ami" "this" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023*"]
+  }
+}
+
+resource "aws_instance" "my-server" {
+  ami           = data.aws_ami.this.id
+  instance_type = "t3.micro"
+
+  key_name = aws_key_pair.deployer.key_name
+
+  network_interface {
+    network_interface_id = aws_network_interface.primary-network-interface.id
+    device_index         = 0
+  }
+
+  tags = {
+    Name = "my-server"
+  }
+}
+
+resource "aws_eip" "eip" {
+  instance = aws_instance.my-server.id
+  vpc      = true
+}
