722849c7c026d5840278105ab2cd1f97f4986b8c
Abdala <clahimoha1000@gmail.com>
2024-05-28 18:03:42 +0300

    created a standalone ingress and egress rules

--- main.tf
+++ main.tf
@@ -88,24 +88,24 @@ resource "aws_security_group" "allow-http" {
   description = "Allow http inbound traffic"
   vpc_id      = aws_vpc.demo-vpc.id
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
   tags = {
     Name = "allow-http"
   }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http" {
+  security_group_id = aws_security_group.allow-http.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  to_port           = 80
+  ip_protocol       = "tcp"
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_http" {
+  security_group_id = aws_security_group.allow-http.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+
 }
 
 resource "aws_security_group" "allow-ssh" {
@@ -113,17 +113,17 @@ resource "aws_security_group" "allow-ssh" {
   description = "Allow ssh inbound traffic from the private subnet"
   vpc_id      = aws_vpc.demo-vpc.id
 
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]
-  }
-
   tags = {
     Name = "allow-ssh"
   }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
+  security_group_id = aws_security_group.allow-http.id
+  cidr_ipv4         = "10.0.0.0/16"
+  from_port         = 22
+  to_port           = 22
+  ip_protocol       = "tcp"
 }
 
 
@@ -168,5 +168,5 @@ resource "aws_instance" "my-server" {
 
 resource "aws_eip" "eip" {
   network_interface = aws_network_interface.primary-network-interface.id
-  vpc      = true
+  vpc               = true
 }
